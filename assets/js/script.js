9edfa5c842574a481022dadfa1245a91d69eef64
ritu-477 <ritujoharbishnoi@gmail.com>
2024-03-28 15:25:25 +0530

    add background layer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 const btnopensidebar = document.querySelector(".open-btn");
 const sidebar = document.querySelector(".sidebar");
 const cross = document.querySelector(".cross");
+const bglayer = document.querySelector(".bglayer");
 
 btnopensidebar.addEventListener("click", function () {
-    sidebar.classList.toggle("show");
+    sidebar.classList.add("show");
+    bglayer.classList.add("d-block");
 })
 cross.addEventListener("click", function () {
-    sidebar.classList.remove("show")
+    sidebar.classList.remove("show");
+    bglayer.classList.remove("d-block");
 })
+
